57634a7c83a105bc3a8570498427d8a78ef543eb
Hitendra singh <hitendrasingh5489@gmail.com>
2025-11-28 11:14:54 +0530

    Class Q3,

---
+++ Lectures - c++/Binary Tree/HeightOfBst.cpp
@@ -0,0 +1,81 @@
+//Heigt of Bst
+#include<iostream>
+using namespace std;
+
+struct Bst{
+    public:
+    int data;
+    Bst* left;
+    Bst* right;
+
+    Bst *root, *temp, *ttemp,*P;
+    void init(){
+        root = temp = ttemp = P = NULL;
+    }
+};
+
+/*************  ✨ Windsurf Command ⭐  *************/
+/**
+ * Creates a new root node with the given data and sets its left and right pointers to NULL.
+ * @param x The data to be stored in the new root node.
+ * @param root A reference to the root node to be created.
+ */
+/*******  843c2fca-fc7f-497d-8b87-8b6c494e15fd  *******/
+void createRoot(int x, Bst* &root){
+    root = new Bst;
+    root->data = x;
+    root->left = root->right = NULL;
+}
+
+void AddNode(int x, Bst* root, Bst* &temp, Bst* &ttemp, Bst* &P){
+    temp = root;
+    while(temp!=NULL){
+        ttemp=temp;
+        if(temp->data > x)
+            temp=temp->left;
+        else
+            temp=temp->right;
+    }
+
+    P = new Bst;
+    P->data = x;
+    P->left = P->right = NULL;
+      if(ttemp->data > x)
+        ttemp->left = P;
+    else
+        ttemp->right = P;
+}
+
+//Height
+int height(Bst* root){
+    if(root == NULL) return 0;
+    int leftht=height(root->left);
+    int rightht=height(root->right);
+    return max(leftht, rightht)+1;
+}
+
+int main(){
+
+
+    Bst obj;
+    obj.init();
+
+ 
+    createRoot(100, obj.root);
+
+    
+    AddNode(50,  obj.root, obj.temp, obj.ttemp, obj.P);
+    AddNode(200, obj.root, obj.temp, obj.ttemp, obj.P);
+    AddNode(20,  obj.root, obj.temp, obj.ttemp, obj.P);
+    AddNode(80,  obj.root, obj.temp, obj.ttemp, obj.P);
+    AddNode(10,  obj.root, obj.temp, obj.ttemp, obj.P);
+    AddNode(30,  obj.root, obj.temp, obj.ttemp, obj.P);
+    AddNode(25,  obj.root, obj.temp, obj.ttemp, obj.P);
+    AddNode(150, obj.root, obj.temp, obj.ttemp, obj.P);
+    AddNode(300, obj.root, obj.temp, obj.ttemp, obj.P);
+    AddNode(110, obj.root, obj.temp, obj.ttemp, obj.P); 
+    AddNode(170, obj.root, obj.temp, obj.ttemp, obj.P);
+
+ cout << "Height of BST = " << height(obj.root) << endl;
+
+}
