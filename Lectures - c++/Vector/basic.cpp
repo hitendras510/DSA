9f0ef4fb664290ae21b75506c89edcd3324c51d8
Hitendra singh <hitendrasingh5489@gmail.com>
2025-12-22 10:44:15 +0530

    . 

---
+++ Lectures - c++/Vector/basic.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+//declare
+vector<int> a;
+cout<<a.size()<<" "<<a.capacity()<<endl;
+a.push_back(1);cout<<a.size()<<" "<<a.capacity()<<endl;    
+
+}
